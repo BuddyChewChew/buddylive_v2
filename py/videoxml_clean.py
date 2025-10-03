409a8149c59e0b4bcfa2102f740d45535cd9eec8
BuddyChewChew <144280901+BuddyChewChew@users.noreply.github.com>
2025-10-03 01:14:46 -0400

    Update videoxml_clean.py

--- py/videoxml_clean.py
+++ py/videoxml_clean.py
@@ -8,8 +8,8 @@ from channel_mapping import channel_names
 def fetch_epgshare01_data():
     """Fetch EPG data from EPGShare01 sources."""
     sources = [
-        "https://epgshare01.online/epgshare01/epg_ripper_US1.xml.gz",
-        "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS2.xml.gz"
+        "https://epgshare01.online/epgshare01/epg_ripper_US2.xml.gz",
+        "https://epgshare01.online/epgshare01/epg_ripper_US_LOCALS1.xml.gz"
     ]
     
     all_programs = {}
